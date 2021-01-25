2cfbd91e253db5f1855a70d65b586e3ad21d9682
Chris Stead <cmstead@gmail.com>
2021-01-25 15:34:31 -0800

    [Refactor] Last bit of refactoring

--- app/Basket.js
+++ app/Basket.js
@@ -3,16 +3,21 @@ class Basket {
         this.items = [];
     }
 
+    getNewItem(description, quantity) {
+        return {
+            description,
+            quantity
+        };
+    }
+
     addItem(description, quantity) {
         const existingItem = this.items.find(item => item.description = description);
-
+        
         if (Boolean(existingItem)) {
             existingItem.quantity++;
         } else {
-            this.items.push({
-                description,
-                quantity
-            });
+            const newItem = this.getNewItem(description, quantity);
+            this.items.push(newItem);
         }
     }
 
