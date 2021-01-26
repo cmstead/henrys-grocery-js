8d86292351719abba515165d6f4fd8270d32da69
Chris Stead <cmstead@gmail.com>
2021-01-25 16:22:15 -0800

    [Refactor] Extracted isInt behavior

--- app/Item.js
+++ app/Item.js
@@ -1,6 +1,12 @@
+const { match } = require('matchlight');
+
 class Item{
     constructor(description, quantity) {
-        if(quantity < 1 || Math.floor(quantity) !== quantity) {
+        function isInt(value) {
+            return Math.floor(value) === value
+        }
+
+        if(quantity < 1 || !isInt(quantity)) {
             throw new Error('Quantity must be an integer that is 1 or greater');
         }
 
