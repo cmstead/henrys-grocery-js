3c92aa10b4dc0b51d89b37d7a83c08d880dc2af4
Chris Stead <cmstead@gmail.com>
2021-01-25 16:26:57 -0800

    [Refactor] Moved to pattern matching for simplified case reading

--- app/Item.js
+++ app/Item.js
@@ -4,18 +4,22 @@ function isInt(value) {
     return Math.floor(value) === value
 }
 
+function isIntGreaterThan0(value) {
+    return isInt(value) && value > 0;
+}
+
 class Item{
     constructor(description, quantity) {
-        function isIntGreaterThan0(value) {
-            return isInt(value) && value > 0;
-        }
-
-        if(!isIntGreaterThan0(quantity)) {
-            throw new Error('Quantity must be an integer that is 1 or greater');
-        }
+        match(quantity, (on, onDefault) => {
+            on(isIntGreaterThan0, 
+                (quantity) => { this.quantity = quantity; });
+            onDefault(
+                () => { 
+                    const message = 'Quantity must be an integer that is 1 or greater';
+                    throw new Error(message); });
+        });
 
         this.description = description;
-        this.quantity = quantity;
     }
 }
 
